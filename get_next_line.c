5d6148b52887e7b02412326b32c6852497b2c9d1
Marcelina Nirska <mnirska@fu-r8-p1.s19.be>
2024-12-07 15:01:21 +0100

    saturday 7/12

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mnirska <mnirska@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 09:26:42 by mnirska           #+#    #+#             */
-/*   Updated: 2024/11/15 14:50:09 by mnirska          ###   ########.fr       */
+/*   Updated: 2024/11/26 21:04:13 by mnirska          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,24 @@
 
 char	*get_next_line(int fd)
 {
+	static char *stash;
+	int			bytes_read;
 	
+	bytes_read = read(fd, stash, BUFFER_SIZE);
+	if (!bytes_read)
+		return (-1)
+	WHILE STASH [I]
+		if stash [i] != \n
+			i++;
+	else 
+	read_line ( )
+
 }
+
 
 int main()
 {
 	int fd;
-	fd = open("text.txt", O_RDONLY);
+	fd = open("file.txt", O_RDONLY);
+	printf()
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,18 @@
 /*   By: mnirska <mnirska@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 09:26:50 by mnirska           #+#    #+#             */
-/*   Updated: 2024/11/15 15:31:51 by mnirska          ###   ########.fr       */
+/*   Updated: 2024/11/26 19:47:53 by mnirska          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 10
 
 # include <fcntl.h>
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-int open (const char* path, int flags [ int mode ]);
-ssize_t read(int fildes, void *buf, size_t nbyte);
-size_t ft_strlen(const char *s);
-char	*ft_substr(const char *str, unsigned int start, size_t len);
+
 
 #endif
